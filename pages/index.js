40d11b80f84c29e3cddbfbd26354f776862c343d
Nizar Ahmad Baihaqi <nizarbaihaq@gmail.com>
2022-03-05 13:36:08 +0700

    Finish Tutorial 🪄

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
+import { getSortedPostsData } from '../lib/posts'
+import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home ({ allPostsData }) {
   return (
     <div className="container">
       <Head>
@@ -14,7 +27,7 @@ export default function Home() {
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Go to <Link href="/posts/first-post">First Post</Link>
         </p>
 
         <div className="grid">
@@ -47,6 +60,23 @@ export default function Home() {
           </a>
         </div>
       </main>
+
+      <section>
+        <h2>Blog</h2>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
 
       <footer>
         <a
