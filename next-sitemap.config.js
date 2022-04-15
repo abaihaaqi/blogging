ec927ec81ac1d9218b21d1a35927558e8a7f3dcf
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-04-16 06:20:01 +0700

    Add next-sitemap package 🛠️

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://blog.nizarbaihaqi.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'monthly',
+  priority: 1,
+}
