7cf2be091fec7a3a68996032710f532bd4a9756c
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-04-16 06:22:27 +0700

    change to weekly

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,6 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://blog.nizarbaihaqi.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  changefreq: 'monthly',
+  changefreq: 'weekly',
   priority: 1,
 }
