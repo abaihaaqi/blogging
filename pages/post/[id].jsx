192ecc45dcf2871830d477d9911edca97ccc6ed6
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-10-14 06:32:26 +0700

    Update post page 🛠️

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -1,38 +1,41 @@
-import Date from '~/components/function/date'
-import Layout from '~/components/Layout'
-import { getAllPostIds, getPostData } from '~/lib/posts'
+import ReactMarkdown from "react-markdown";
+import Date from "~/components/function/date";
+import Layout from "~/components/Layout";
+import { getAllPostIds, getPostData } from "~/lib/posts";
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id);
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
-export default function Post ({ postData }) {
+export default function Post({ postData }) {
   const metaContent = {
     url: postData.id,
     title: postData.title,
     description: postData.desc,
-    img: postData.img
-  }
+    img: postData.img,
+  };
 
   return (
-    <Layout title={postData.title} img={postData.img} metaContent={metaContent}>
-      <div className="py-4">
-        <Date dateString={postData.date} />
+    <Layout title={postData.title} img={postData.img} topics={postData.topics} metaContent={metaContent}>
+      <div className="bg-zinc-900 rounded-lg p-4">
+        <p className="py-0">
+          {postData.author} - <Date dateString={postData.date} />
+        </p>
+        <ReactMarkdown children={postData.content} />
       </div>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
-  )
+  );
 }
