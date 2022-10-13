a88916dd49231084f54854285fd5871f846439fe
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-10-14 06:29:29 +0700

    Update default style 🛠️

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
     fontSize: {
-      'sm': '0.9rem',
-      'base': '1rem',
-      'lg': '2rem',
-      'xl': '2.5rem',
-      '2xl': '3rem',
-      '3xl': '4rem'
+      sm: "0.9rem",
+      base: "1rem",
+      md: "1.5rem",
+      lg: "2rem",
+      xl: "2.5rem",
     },
     container: {
-      center: true
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+        serif: ["Lora", "serif"],
+        mono: ["Fira Code", "monospace"],
+      },
     },
   },
   plugins: [],
-}
+};
