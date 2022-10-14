c11b5ea9e37e6499095e4a7b1be9a5ad6a9e8e27
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-10-14 07:34:32 +0700

    Add disqus comments 🛠️

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -2,6 +2,7 @@ import ReactMarkdown from "react-markdown";
 import Date from "~/components/function/date";
 import Layout from "~/components/Layout";
 import { getAllPostIds, getPostData } from "~/lib/posts";
+import Disqus from "disqus-react";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -28,13 +29,28 @@ export default function Post({ postData }) {
     img: postData.img,
   };
 
+  const shortname = "nizar-baihaqi";
+  const disqusConfig = {
+    url: "https://blog.nizarbaihaqi.com",
+    identifier: postData.id,
+    title: postData.title,
+  };
+
   return (
-    <Layout title={postData.title} img={postData.img} topics={postData.topics} metaContent={metaContent}>
-      <div className="bg-zinc-900 rounded-lg p-4">
+    <Layout
+      title={postData.title}
+      img={postData.img}
+      topics={postData.topics}
+      metaContent={metaContent}
+    >
+      <div className="bg-zinc-900 rounded-lg p-4 mb-3">
         <p className="py-0">
           {postData.author} - <Date dateString={postData.date} />
         </p>
         <ReactMarkdown children={postData.content} />
+      </div>
+      <div className="bg-white rounded-lg px-3">
+        <Disqus.DiscussionEmbed shortname={shortname} config={disqusConfig} />
       </div>
     </Layout>
   );
