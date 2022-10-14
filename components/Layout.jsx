3bf8f42430dc3270643edd7168ed623955cb5961
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-10-14 19:44:13 +0700

    Add search functionality 🛠️

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,9 @@
 import Image from "next/image";
-import { useRouter } from "next/router";
 import MetaTags from "./MetaTags";
 import Navbar from "./Navbar";
 import defaultImg from "~/public/images/default.png";
-import {
-  GithubLogo,
-  InstagramLogo,
-  LinkedinLogo,
-  MagnifyingGlass,
-} from "phosphor-react";
-
-// Implement search with algolia
-import algoliasearch from "algoliasearch";
-import { InstantSearch, SearchBox } from "react-instantsearch-hooks-web";
+import { GithubLogo, LinkedinLogo } from "phosphor-react";
 import Link from "next/link";
-
-const searchClient = algoliasearch(
-  "KE5KMEQDE6",
-  "2e5a27e4d94aff7d3b6cb0f482f17f1a"
-);
 
 export default function Layout({
   children,
@@ -28,12 +13,6 @@ export default function Layout({
   topics,
   metaContent,
 }) {
-  const router = useRouter();
-
-  const splitString = (str) => {
-    return str.split(`, `);
-  };
-
   return (
     <>
       <MetaTags {...metaContent} />
@@ -48,17 +27,6 @@ export default function Layout({
         <div className="absolute inset-0 bg-black/70 backdrop-blur-md" />
         <div className="flex flex-col justify-center items-center text-center max-w-4xl mx-auto px-2 absolute inset-0 z-10">
           <h1 className="font-serif font-normal 2xl:text-2xl mb-6">{title}</h1>
-          {/* {home ? (
-            <form className="flex items-stretch gap-2 h-[38px]">
-              <input type="text" placeholder="Search" />
-              <button
-                type="submit"
-                className="bg-zinc-900 rounded px-2 hover:bg-zinc-800 transition-all"
-              >
-                <MagnifyingGlass size={24} />
-              </button>
-            </form>
-          ) :  */}
           {!home && (
             <>
               <div className="flex gap-2">
